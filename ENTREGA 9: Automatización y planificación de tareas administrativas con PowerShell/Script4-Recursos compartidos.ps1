b755560401cd1fe852beed563ff80565280dbf24
juancarloscrosiespicasso <jcroflo602.picasso@gmail.com>
2026-02-20 10:19:24 +0100

    Script4-Recursos compartidos.ps1

--- ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/Script4-Recursos compartidos.ps1
+++ ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/Script4-Recursos compartidos.ps1
@@ -5,4 +5,7 @@ foreach($carpeta in $carpetas){
     Write-Host "Nombre: $($carpeta.Name)"
 }
 
+
 Get-SmbShareAccess -Name $($carpeta.Name) | Out-File -FilePath C:\ResultadoScripts\InfoConectividad.txt -Append
+
+#Juan Carlos Cros Flor
