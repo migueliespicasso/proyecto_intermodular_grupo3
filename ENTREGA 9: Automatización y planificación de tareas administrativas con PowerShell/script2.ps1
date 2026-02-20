2274b314f16be46917923671c4d823b3abcdbbb0
davidmunoziespicasso <dmunhed477@g.educaand.es>
2026-02-20 09:35:27 +0100

    Add files via upload

---
+++ ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/script2.ps1
@@ -0,0 +1,23 @@
+﻿$Grupo = Read-Host "Pon el nombre el grupo"
+$Ruta = "C:\ResultadoScripts"
+$Archivo = "$Ruta\InfoUsuarios.txt"
+
+if (!(Test-Path $Ruta)) {
+New-Item -ItemType Directory -Path $Ruta
+}
+
+Import-Module ActiveDirectory
+
+$DN = (Get-ADGroup $Grupo).DistinguishedName
+
+$Resultado = Get-ADUser -Filter * -Properties MemberOf, Enabled, ManagedBy |
+Where-Object { $_.ManagedBy -eq $DN } |
+ForEach-Object {
+$Grupos = ($_.MemberOf | ForEach-Object { (Get-ADGroup $_).Name }) -join ", "
+"$($_.SamAccountName) | $Grupos | " + ($(if($_.Enabled){"Habilitado"}else{"Deshabilitado"}))
+}
+
+
+$Resultado
+
+$Resultado | Out-File $Archivo
