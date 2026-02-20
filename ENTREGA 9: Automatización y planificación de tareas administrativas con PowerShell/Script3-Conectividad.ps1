4ba85cb86044bc1562db0ee0f5bafdef895f69df
usuario <jcroflo602.picasso@gmail.com>
2026-02-20 10:13:26 +0100

    scripts

---
+++ ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/Script3-Conectividad.ps1
@@ -0,0 +1,23 @@
+﻿$Servidor = "192.168.1.100"
+$Cliente = "192.168.1.101"
+$Router = "192.168.1.102"
+
+function Comprobar-Conectividad {
+    param (
+        [string]$Nombre,
+        [string]$Direccion
+    )
+    if (Test-Connection -ComputerName $Direccion -Count 2 -Quiet) { 
+        $test= "$Nombre ($Direccion): Conectado" 
+        $test | Out-File -FilePath C:\ResultadoScripts\InfoConectividad.txt -Append
+    }
+    else {
+        $test= "$Nombre ($Direccion): No conectado"
+        $test | Out-File -FilePath C:\ResultadoScripts\InfoConectividad.txt -Append
+    }
+}
+
+Get-Date -Format "dd/MM/yyyy" | Out-File -FilePath C:\ResultadoScripts\InfoConectividad.txt -Append
+Comprobar-Conectividad -Nombre "Servidor" -Direccion $Servidor  
+Comprobar-Conectividad -Nombre "Cliente" -Direccion $Cliente 
+Comprobar-Conectividad -Nombre "Router" -Direccion $Router
---
+++ ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/Script4-Recursos compartidos.ps1
@@ -0,0 +1,8 @@
+Get-SmbShare | Out-File -FilePath C:\ResultadoScripts\InfoConectividad.txt -Append
+
+$carpetas=smbShare
+foreach($carpeta in $carpetas){
+    Write-Host "Nombre: $($carpeta.Name)"
+}
+
+Get-SmbShareAccess -Name $($carpeta.Name) | Out-File -FilePath C:\ResultadoScripts\InfoConectividad.txt -Append
