c405bf3b2f54ea60ec8b383534989c33265fcdd5
davidmunoziespicasso <dmunhed477@g.educaand.es>
2026-02-20 08:31:27 +0100

    EntregaActividades

---
+++ ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/Script5.ps1
@@ -0,0 +1,6 @@
+﻿mkdir C:\ResultadosScripts -ErrorAction SilentlyContinue
+
+New-SmbShare -Name ResultadoScripts -Path C:\ResultadosScripts -ReadAccess "Todos" -ErrorAction SilentlyContinue
+
+Get-Printer >> C:\ResultadosScripts\InformacionImpresora.txt
+Get-WmiObject Win32_Printer > C:\ResultadosScripts\InformacionPermisosImpresora.txt
---
+++ ENTREGA 9: Automatización y planificación de tareas administrativas con PowerShell/Script6.ps1
@@ -0,0 +1,5 @@
+﻿Get-PSDrive > C:\ResultadosScripts\Script6.txt
+type C:\ResultadosScripts\Script6.txt
+
+Write-Host "Último inicio del sistema:"
+(Get-CimInstance Win32_OperatingSystem).LastBootUpTime
